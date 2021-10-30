a9ac7a6fcd162b2a612741f6512ef55f857875d4
a.k <a@b.com>
2021-10-30 20:42:50 +0800

    fix: proto_decoder cannot decode binary file

--- proto_decoder/proto_decoder.go
+++ proto_decoder/proto_decoder.go
@@ -12,17 +12,8 @@ import (
 	"strings"
 
 	"github.com/aj3423/aproto"
-
-	"github.com/fatih/color"
 )
 
-func ReadFile(fn string) []byte {
-	s, err := ioutil.ReadFile(fn)
-	if err != nil {
-		return []byte{}
-	}
-	return s
-}
 func UnZlib(b []byte) ([]byte, error) {
 	z, err := zlib.NewReader(bytes.NewReader(b))
 	if err != nil {
@@ -32,20 +23,18 @@ func UnZlib(b []byte) ([]byte, error) {
 	return ioutil.ReadAll(z)
 }
 
-func has_flag(name string) bool {
-	found := false
-	flag.Visit(func(f *flag.Flag) {
-		if f.Name == name {
-			found = true
-		}
-	})
-	return found
+func error_assert(e error) {
+	if e != nil {
+		fmt.Println("Error:")
+		fmt.Println(e.Error())
+		os.Exit(1)
+	}
 }
 
 func main() {
 
 	file := flag.String("file", "", "file contain proto like 080d....")
-	flag.Bool("bin", false, "content is binary")
+	is_bin := flag.Bool("bin", false, "content is binary")
 	is_b64 := flag.Bool("b64", false, "content is base64")
 	is_zlib := flag.Bool("zlib", false, "content is zlib")
 	is_xxd := flag.Bool("xxd", false, "content is hexdump (without offset and text)")
@@ -56,27 +45,27 @@ func main() {
 		return
 	}
 
+	var e error
+
 	var proto []byte // binary
 
 	// 1. file ?
-	if has_flag("file") {
-		proto = ReadFile(*file)
+	if *file != "" {
+		proto, e = ioutil.ReadFile(*file)
+		error_assert(e)
 	} else {
 		proto = []byte(flag.Arg(0)) // last param
 	}
 
 	// base64 ?
 	if *is_b64 {
-		var e error
 		proto, e = base64.StdEncoding.DecodeString(string(proto))
-		if e != nil {
-			color.HiRed(e.Error())
-			return
-		}
+		error_assert(e)
 	} else {
 		// binary ?
-		if !has_flag("bin") {
-			proto, _ = hex.DecodeString(string(proto))
+		if !*is_bin {
+			proto, e = hex.DecodeString(string(proto))
+			error_assert(e)
 		}
 	}
 
@@ -84,10 +73,7 @@ func main() {
 	if *is_zlib {
 		var e error
 		proto, e = UnZlib(proto)
-		if e != nil {
-			color.HiRed(e.Error())
-			return
-		}
+		error_assert(e)
 	}
 
 	if strings.ContainsAny(flag.Arg(0), " \r\n") {
@@ -101,12 +87,12 @@ func main() {
 		s = strings.ReplaceAll(s, "\n", "")
 		s = strings.ReplaceAll(s, "\r", "")
 		s = strings.ReplaceAll(s, "\t", "")
-		proto, _ = hex.DecodeString(s)
+		proto, e = hex.DecodeString(s)
+		error_assert(e)
 	}
 
-	p, err := aproto.TryDump(proto)
-	if err != nil {
-		panic(err)
-	}
+	p, e := aproto.TryDump(proto)
+	error_assert(e)
+
 	fmt.Println(p)
 }
